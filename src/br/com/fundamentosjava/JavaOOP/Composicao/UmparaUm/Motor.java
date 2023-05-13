87efae0f52dd735d2654ab8ae7c3c616ade97930
lucas <lcs_demais@hotmail.com>
2023-05-13 12:05:49 -0300

    att

---
+++ src/br/com/fundamentosjava/JavaOOP/Composicao/UmparaUm/Motor.java
@@ -0,0 +1,19 @@
+package br.com.fundamentosjava.JavaOOP.Composicao.UmparaUm;
+
+public class Motor {
+    boolean ligado = false;
+    double fatorInjecao = 1;
+    final Carro carro;
+
+    Motor(Carro carro){
+        this.carro = carro;
+    }
+    int giros(){
+
+        if(!ligado){
+            return 0;
+        }else {
+            return (int) Math.round(fatorInjecao * 3000);
+        }
+    }
+}
