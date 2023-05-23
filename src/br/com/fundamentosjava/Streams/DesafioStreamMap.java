c3a52dc9f86856ad8bf9091af4203f3f2ea28225
lucas <lcs_demais@hotmail.com>
2023-05-23 19:03:08 -0300

    att

--- src/br/com/fundamentosjava/Streams/DesafioStreamMap.java
+++ src/br/com/fundamentosjava/Streams/DesafioStreamMap.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
-public class DesafioStramMap {
+public class DesafioStreamMap {
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
--- src/br/com/fundamentosjava/Streams/criandoStreams.java
+++ src/br/com/fundamentosjava/Streams/criandoStreams.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class criandoStrams {
+public class criandoStreams {
     public static void main(String[] args) {
 
         System.out.println("Método Stream normal");
