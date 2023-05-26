69f287819b48776be68a7c58caadd5143cf895ef
lucas <lcs_demais@hotmail.com>
2023-05-26 17:03:47 -0300

    att

---
+++ src/br/com/fundamentosjava/ErrosExecoes/ChecadaVsNaoChecada.java
@@ -0,0 +1,37 @@
+package br.com.fundamentosjava.ErrosExecoes;
+
+public class ChecadaVsNaoChecada {
+    public static void main(String[] args) {
+
+        try{
+            geraError1();
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            geraError2();
+        }catch (Throwable e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+        System.out.println("aa");
+    }
+
+    // não checada ou nao verifica
+    static void geraError1(){
+        throw new RuntimeException("A fudeo erro 1");
+    }
+
+
+    // checada
+    static void geraError2() throws Exception {
+      try {
+          throw new Exception("erro ahhh 2");
+      }catch (Exception e){
+          System.out.println(e.getMessage());
+      }
+    }
+}
