926c1b4cab807df3b487e4e04379ebe0b86d4019
lucas <lcs_demais@hotmail.com>
2023-05-31 16:38:30 -0300

    att

---
+++ src/br/com/fundamentosjava/JDBC/TesteConexao.java
@@ -0,0 +1,20 @@
+package br.com.fundamentosjava.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) throws SQLException {
+
+        String stringDeConexao = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+        String usuario = "root";
+        String senha = "123456789";
+
+        Connection conexao = DriverManager
+                .getConnection(stringDeConexao,usuario,senha);
+
+        System.out.println("Deu boa");
+        conexao.close();
+    }
+}
