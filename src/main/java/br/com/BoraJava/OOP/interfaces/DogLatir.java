d6702dd4beeb30c089eff420b5b10d52a8e276b2
lucas <lcs_demais@hotmail.com>
2023-06-29 12:50:07 -0300

    classes de interface

---
+++ src/main/java/br/com/BoraJava/OOP/interfaces/DogLatir.java
@@ -0,0 +1,16 @@
+package br.com.BoraJava.OOP.interfaces;
+
+public interface DogLatir {
+
+    public abstract void latir();
+
+    // método default permite fazer um método sem que a class
+    // seja obrigada a ter o método também
+    default void naoprecisa(){
+        System.out.println("Essa não precisa implementar");
+    }
+
+    public static void doglatir(){
+        System.out.println("Metodo static de dentro de doglatir");
+    }
+}
---
+++ src/main/java/br/com/BoraJava/OOP/interfaces/DogRaca.java
@@ -0,0 +1,10 @@
+package br.com.BoraJava.OOP.interfaces;
+
+public interface DogRaca {
+
+    public abstract void raca();
+
+    public static void dograca(){
+        System.out.println("Metodo static dentro de dograca");
+    }
+}
