f0873b957aa8f7755255a6b23de5d323bf5cc158
lucas <lcs_demais@hotmail.com>
2023-07-10 14:55:10 -0300

    regex

---
+++ src/main/java/br/com/BoraJava/Regex/PatternMatcherTest.java
@@ -0,0 +1,21 @@
+package br.com.BoraJava.Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest {
+    public static void main(String[] args) {
+        String regex = "ab";
+        String texto = "ababalalabala";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:   "+ texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+ regex);
+        System.out.println("Posicoes econtradas");
+        while(matcher.find()){
+            System.out.println(matcher.start()+" ");
+        }
+    }
+}
---
+++ src/main/java/br/com/BoraJava/ResourceBundle/ResourceBundleTeste01.java
@@ -0,0 +1,20 @@
+package br.com.BoraJava.ResourceBundle;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTeste01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("messages",new Locale("pt","BR"));
+        boolean sasa = bundle.containsKey("sasa");
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        bundle = ResourceBundle.getBundle("messages",new Locale("en","US"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+
+    }
+}
